2c3b5b1dcc2f90e5f28e7bd77ebafc744c09c7e7
XiaoXiaoSN <xiao.xiao@tenoz.tw>
2024-01-09 04:18:47 +0000

    feat: allow logging agent to log Json value

--- SDK/agents/Logging.cs
+++ SDK/agents/Logging.cs
@@ -1,8 +1,14 @@
-﻿using Saffron.Runtime;
+﻿using Google.Protobuf.WellKnownTypes;
+using Saffron.Runtime;
 
 public static class LoggingAgent
 {
     public async static Task Trace(string message)
+    {
+        await Log("TRACE", message);
+    }
+
+    public async static Task Trace(object message)
     {
         await Log("TRACE", message);
     }
@@ -12,7 +18,17 @@ public static class LoggingAgent
         await Log("DEBUG", message);
     }
 
+    public async static Task Debug(object message)
+    {
+        await Log("DEBUG", message);
+    }
+
     public async static Task Info(string message)
+    {
+        await Log("INFO", message);
+    }
+
+    public async static Task Info(object message)
     {
         await Log("INFO", message);
     }
@@ -22,7 +38,17 @@ public static class LoggingAgent
         await Log("WARN", message);
     }
 
+    public async static Task Warn(object message)
+    {
+        await Log("WARN", message);
+    }
+
     public async static Task Error(string message)
+    {
+        await Log("ERROR", message);
+    }
+
+    public async static Task Error(object message)
     {
         await Log("ERROR", message);
     }
@@ -36,7 +62,20 @@ public static class LoggingAgent
         {
             TaskKey = Global.TaskKey.ToProto(),
             Level = level,
-            Message = message,
+            Plaintext = message,
+        });
+    }
+
+    public async static Task Log(string level, object message)
+    {
+        var channel = GrpcChannelService.GetChannel();
+        var client = new Runtime.RuntimeClient(channel);
+
+        await client.LogAsync(new LogRequest
+        {
+            TaskKey = Global.TaskKey.ToProto(),
+            Level = level,
+            Json = Utils.ConvertObjectToValue(message),
         });
     }
 }
